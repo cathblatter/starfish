fcdced46bd0605e18d482cad54a9ddf3e379e7d7
Catherine Blatter <catherine.blatter@unibas.ch>
2023-02-01 13:42:57 +0100

    welcome underwater! 🐳🐙🌊🪸

---
+++ R/internals-ins.R
@@ -0,0 +1,17 @@
+#' Internal directories
+#' @description paths to the project directory
+#' @export
+wd_rd <- "/Volumes/research$/STARFISH/"
+
+
+#' Internal directories
+#' @description paths to the project directory - data folder
+#' @export
+wd_data_rd <- "/Volumes/research$/STARFISH/01_data/"
+
+
+#' Internal directories
+#' @description paths to the project directory - de-id'd data folder
+#' @export
+wd_deidata_rd <- "/Volumes/research$/STARFISH/01_data/00_deidentified-exports/"
+
---
+++ README.md
@@ -0,0 +1,14 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# starfish
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+<!-- badges: end -->
+
+- contains internal helpers
+
+- heavily under development
