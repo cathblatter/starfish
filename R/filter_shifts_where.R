0072db0a1faa26ea66d604504f2fb3b5465fffe5
Catherine Blatter <catherine.blatter@unibas.ch>
2023-06-01 15:31:55 +0200

    update documentation

--- R/filter_shifts_where.R
+++ R/filter_shifts_where.R
@@ -1,12 +1,12 @@
 #' Filter all shifts where a certain condition is met
 #'
-#' @param .data
+#' @param .data a data frame or tibble
 #' @param ... condition to pass to dplyr::filter
+#' @param shift_var variable with the shift_identified, default = "shift_id"
 #'
 #' @return a dataframe containing all shift-entries where a condition is met
 #' @export
 #'
-#' @examples
 filter_shifts_where <- function(.data, ..., shift_var = shift_id){
 
   shift_id <- NULL
